017f683d2d2850b5107662a02288edd2f5674182
Gustavo Vicente Ozorio <gustavoozorio1227@hotmail.com>
2023-07-20 01:01:32 -0300

    Add files via upload

--- db_chamados.sql
+++ db_chamados.sql
@@ -17,15 +17,16 @@ CREATE TABLE statuses (
 CREATE TABLE tickets (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  category_id INT,
+  category_id varchar(15),
   description TEXT,
   deadline DATE,
-  status_id INT DEFAULT 1,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  status_id varchar(15) default'Novo',
+  created_at date,
   solved_at DATE,
   created_by INT,
   handled_by INT
 );
 Drop table tickets;
 select * from tickets;
+
 INSERT INTO statuses (name) VALUES ('Novo'), ('Em atendimento'), ('Resolvido');
